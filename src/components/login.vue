e0e04591e001c7efc3f8a0b75f27c4f0820c49b5
batuld_webelight <batuld@webelight.co.in>
2023-10-11 11:41:09 +0530

    feat: developed login page with tailwind css

---
+++ src/components/login.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import axios from "../plugins/axios"
+
+const loginInfo = reactive({
+  email: '',
+  password: ''
+})
+
+const submit = async () => {
+  await axios.post(api).then((response) => {
+  console.log(response.data)
+})
+} 
+</script>
+
+<template>
+  
+<div class="flex justify-center bg-yellow-100">
+
+  <div class="grid grid-cols-1 mt-10">
+  <div>Email address: <input v-model="loginInfo.email" class="border-2 w-52"/></div>
+  <div class="mt-5">Password: <input v-model="loginInfo.password" class="border-2 w-52"/></div>
+  <button @click="submit()">Submit</button>
+</div>
+</div>
+</template>
